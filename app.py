d65afbbe2d5eb2d1cb972ed68a37f78e25ac5c79
solominh <hoangminhftu2@gmail.com>
2017-01-03 19:14:03 +0700

    Chrome web driver

---
+++ app.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+
+url='http://vnexpress.net/tin-tuc/thoi-su/cong-ngan-trieu-bung-nap-o-sai-gon-co-dau-hieu-bi-cay-pha-3522886.html'
+browser = webdriver.Chrome()
+browser.get(url)
