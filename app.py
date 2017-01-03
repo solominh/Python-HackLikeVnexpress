5c12c89f17a0d8f58ada15782c672b04421e1db3
solominh <hoangminhftu2@gmail.com>
2017-01-03 20:44:28 +0700

    Hack like success

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from selenium import webdriver
 
-url='http://vnexpress.net/tin-tuc/thoi-su/cong-ngan-trieu-bung-nap-o-sai-gon-co-dau-hieu-bi-cay-pha-3522886.html'
 browser = webdriver.Chrome()
+# browser = webdriver.Firefox()
+
+# URL
+url = 'http://vnexpress.net/tin-tuc/thoi-su/cong-ngan-trieu-bung-nap-o-sai-gon-co-dau-hieu-bi-cay-pha-3522886.html'
 browser.get(url)
+
+# Get second element of same id group
+element = browser.find_element_by_xpath('//*[@id="19822386"][2]')
+element.click()
+
